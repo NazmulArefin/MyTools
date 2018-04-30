4c8e7599d2b0ca3ec7893ce37c04df1752587736
Nazmul <nazmul.arefin.85@gmail.com>
2018-04-30 18:22:47 +0600

    new

--- NecessaryTools/Controllers/Alyaradat Walmasrufat/AlMasrufatController.cs
+++ NecessaryTools/Controllers/Alyaradat Walmasrufat/AlMasrufatController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NecessaryTools.Models.Alyaradat_Walmasrufat;
 
 namespace NecessaryTools.Controllers.Alyaradat_Walmasrufat
 {
@@ -13,5 +14,11 @@ namespace NecessaryTools.Controllers.Alyaradat_Walmasrufat
         {
             return View();
         }
+
+        //public ActionResult Save(AlMasrufat alMasrufat)
+        //{
+            
+        //}
     }
+    
 }
--- NecessaryTools/Models/Alyaradat Walmasrufat/AlMasrufat.cs
+++ NecessaryTools/Models/Alyaradat Walmasrufat/AlMasrufat.cs
@@ -10,14 +10,21 @@ namespace NecessaryTools.Models.Alyaradat_Walmasrufat
     {
         public int ExpenseId { get; set; }
         [DisplayName(@"Source Name:")]
-        public string ExpenseSource { get; set; }
+        public string ExpSource { get; set; }
         public string Goods { get; set; }
         public int Quantity { get; set; }
+        public string Unit { get; set; }
         public double Amount { get; set; }
         public double Total { get; set; }
         [DisplayName(@"Date of Expense:")]
         public string ExpDate { get; set; }
         public string Remarks { get; set; }
+
+
+        //public static string Save(AlMasrufat alMasrufat)
+        //{
+            
+        //}
 
     }
 }
